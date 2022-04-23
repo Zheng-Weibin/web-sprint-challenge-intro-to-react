27161757b96e206ffb9fe8f93e5a0f3916d59ae7
Weibin21 <weibinzheng21@gmail.com>
2022-04-23 18:14:55 -0400

    updated Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+  background-color: gray;
+  opacity: 0.8;
+  border: 3px solid darkgray;
+  border-radius: 25px;
+`;
+
+const Character = (props) => {
+  const { character } = props;
+
+  return (
+    <StyledCharacter className="sw-characters">
+      <h2>{character.name}</h2>
+      {character.films.length > 0
+        ? character.films.map((film) => {
+            return <p>{film}</p>;
+          })
+        : ""}
+    </StyledCharacter>
+  );
+};
+
+export default Character;
